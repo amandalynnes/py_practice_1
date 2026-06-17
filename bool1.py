f3de1a860bfe183da6181908d75e9171ea914c3c
Amanda S. <69062576+amandalynnes@users.noreply.github.com>
2026-06-16 21:20:14 -0400

    Uploaded 'bool1' and 'loop_prep' .py files
    Added boolean practice as well as for/while loop exam prep exercises.

---
+++ bool1.py
@@ -0,0 +1,7 @@
+c = 3
+d = 3
+equal_value = (c == d)
+same_identity = (d is c)
+
+print(f"c and d are equal in value: {equal_value}. d and c are the same: {same_identity}")
+
---
+++ loop_prep.py
@@ -0,0 +1,9 @@
+# For and while loop exam prep
+# count = 1
+
+# while count <= 5:
+#     print(count)
+#     count += 1
+
+for i in range(2, 12, 2):
+    print(i)
